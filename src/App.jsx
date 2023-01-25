3fea3ff5969a9989b9bcf52e878e25b5f0b2e3a4
kenneth <kenneth1.kodehode@gmail.com>
2023-01-25 15:06:27 +0100

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <>
+      <div className="App">
+        <div className="card w-[500px] bg-base-200 shadow-xl">
+          <figure>
+            <img src="/src/images/image-equilibrium.jpg" />
+          </figure>
+          <div className="card-body text-center">
+            <h2 className="card-title text-2xl text-sky-500 ">
+              Equilibrium #3492
+            </h2>
+            <p className="text-left text-xl">
+              Our Equilibrium collection promotes balance and calm!
+            </p>
+            <div className="card-actions justify-end"></div>
+            <div className="flex">
+              <img src="/src/images/icon-ethereum.svg" className="w-[12px]" />
+              <p>0.041ETH</p>
+              <p>3 days left</p>
+              <img src="/src/images/icon-clock.svg" />
+            </div>
+            <hr></hr>
+            <div className="flex mt-20">
+              <img src="/src/images/image-avatar.png" className="w-20 -mt-10" />
+              <p>Creation of Kenneth</p>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
